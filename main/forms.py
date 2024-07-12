1410c2ec8e5250ae861171251e72ad5d4a1b4ad7
Abduvohid26 <abdujalilovabduvohid6@gmail.com>
2024-07-12 08:56:43 +0500

    model fixed

--- main/forms.py
+++ main/forms.py
@@ -2,4 +2,14 @@ from django import forms
 from .models import Subject
 
 class SubjectSelectForm(forms.Form):
-    subject = forms.ModelChoiceField(queryset=Subject.objects.all(), required=False, label="Fanni tanlang", initial='Fan tanlang')
+    subject = forms.ModelChoiceField(
+        queryset=Subject.objects.all(),
+        required=False,
+        label="Fanni tanlang",
+        # empty_label="Fan tanlang"
+    )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if Subject.objects.exists():
+            self.fields['subject'].initial = Subject.objects.first()
